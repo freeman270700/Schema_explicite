98abca81447a562d648892d05dffa075699bd6fa
freeman270700 <mehyouyou@gmail.com>
2022-12-05 16:29:06 +0100

    push

--- init.py
+++ init.py
@@ -9,30 +9,47 @@ dz = 0.25
 Nz = 400
 Nt = 5000
 dt = (365*24*60*60)/Nt
-temp = np.ones((Nz+1, Nt+1))
-pas = 12/Nt
-u = 15 * temp
-for i in range(0, Nt+1):
-    u[0, i] = 15 - 10 * sin(2*pi*i*pas/12)
+u = np.ones((Nz, Nt))
+#affiche la taille de temp
+temp = np.linspace(0,12,Nt)
+u = 15 * u
+#pour i allant de 0 à Nt
+for i in range(0,Nt):
+    u[0, i] = 15 - 10 * sin(2*pi*temp[i]/12)
 maxiter = 500
-for iter in range(0 , maxiter) : 
+for iter in range(0 , maxiter-1) : 
     uold = u
     u[:,0] = uold[:,-1]
-    for i in range(1, Nt+1):
-        profondeur = (u[0:(Nz)-2,i-1]) 
-        profondeur=- 2*(u[1:(Nz)-1,i-1])
-        profondeur=+ (u[2:(Nz),i-1])
+    for i in range(1, Nt):
+        profondeur = (u[0:(Nz)-3,i-1]) 
+        profondeur=- 2*(u[1:(Nz-2),i-1])
+        profondeur=+ (u[2:(Nz-1),i-1])
+        profondeur= profondeur/(dz**2)
+       # profondeur = profondeur
         temps_1D = K*profondeur  
-        u[1:Nz,i] =+ u[1:Nz,i-1] 
-        u[2:Nz,i]=+ temps_1D
-        u[-1,i] = u[len(u)-1,i]
-    #trouver le maximum en valeur absolue entre deux solutions
-    diff = abs(uold - u)
-    diffmax = diff.max()
-    print(diffmax)
-    if diffmax < 1e-4 : 
+        u[1:Nz-2,i] =+ u[1:Nz-2,i-1] 
+        u[1:Nz-2,i]=+ temps_1D*dt
+        u[Nz-1,i] = u[Nz-2,i]
+    #trouver le maximum en valeur absolue entre deux solutions u et uold
+    diff = np.max(np.abs(u-uold))
+    if(diff < 1e-4):
         break
-    end
+
 #afficher valeur err
-print(diffmax)
-#sortie graphique
+
+#sortie graphique variation de la température en fonction de la profondeur
+plt.figure()
+#mettre les abscisses en fcontion du temps et les ordonnées en fonction de la temperature
+
+print(u[1,:])
+#mettre les ordonnées entre 0 et 15
+plt.ylabel('Température (°C)')
+#afficher la courbe
+#plt.plot(temp,u[0,:], 'r')
+plt.plot(temp,u[20,:], 'r')
+#plt.plot(temp,u[40,:], 'b')
+#plt.plot(temp,u[60,:], 'b')
+#plt.plot(temp,u[80,:], 'g')
+plt.show()
+
+
