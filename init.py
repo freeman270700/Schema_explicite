adccb3e23020e94576422109b47b0f184ba949f9
freeman270700 <mehyouyou@gmail.com>
2022-12-05 17:26:20 +0100

    update

--- init.py
+++ init.py
@@ -9,26 +9,21 @@ dz = 0.25
 Nz = 400
 Nt = 5000
 dt = (365*24*60*60)/Nt
-u = np.ones((Nz, Nt))
+u = np.ones((Nz+1, Nt+1))
 #affiche la taille de temp
-temp = np.linspace(0,12,Nt)
+temp = np.linspace(0,12,Nt+1)
 u = 15 * u
 #pour i allant de 0 à Nt
-for i in range(0,Nt):
+for i in range(0,Nt+1):
     u[0, i] = 15 - 10 * sin(2*pi*temp[i]/12)
 maxiter = 500
-for iter in range(0 , maxiter-1) : 
+for iter in range(0 , maxiter) : 
     uold = u
     u[:,0] = uold[:,-1]
-    for i in range(1, Nt):
-        profondeur = (u[0:(Nz)-3,i-1]) 
-        profondeur=- 2*(u[1:(Nz-2),i-1])
-        profondeur=+ (u[2:(Nz-1),i-1])
-        profondeur= profondeur/(dz**2)
-       # profondeur = profondeur
+    for i in range(1, Nt+1):
+        profondeur = ((u[0:(Nz-2),i-1]) - 2*(u[1:(Nz-1),i-1]) + (u[2:(Nz),i-1]))/dz**2
         temps_1D = K*profondeur  
-        u[1:Nz-2,i] =+ u[1:Nz-2,i-1] 
-        u[1:Nz-2,i]=+ temps_1D*dt
+        u[1:Nz-2,i] = dt*temps_1D[1:Nz-2] + u[1:Nz-2,i-1] 
         u[Nz-1,i] = u[Nz-2,i]
     #trouver le maximum en valeur absolue entre deux solutions u et uold
     diff = np.max(np.abs(u-uold))
@@ -41,15 +36,14 @@ for iter in range(0 , maxiter-1) :
 plt.figure()
 #mettre les abscisses en fcontion du temps et les ordonnées en fonction de la temperature
 
-print(u[1,:])
+print(u[2,:])
 #mettre les ordonnées entre 0 et 15
 plt.ylabel('Température (°C)')
-#afficher la courbe
-#plt.plot(temp,u[0,:], 'r')
+plt.plot(temp,u[0,:], 'r')
 plt.plot(temp,u[20,:], 'r')
-#plt.plot(temp,u[40,:], 'b')
-#plt.plot(temp,u[60,:], 'b')
-#plt.plot(temp,u[80,:], 'g')
+plt.plot(temp,u[40,:], 'b')
+plt.plot(temp,u[60,:], 'b')
+plt.plot(temp,u[80,:], 'g')
 plt.show()
 
 
